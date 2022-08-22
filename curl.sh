e4b23b48e7121fdd5ba17a5bedd876bb0b636597
Cédric Della Casa <cdellacasa@openwt.com>
2022-08-22 11:07:12 +0200

    updated curl for easier testing

--- curl.sh
+++ curl.sh
@@ -1,23 +1,38 @@
-select action in run poll end 
+select action in run poll end run1 poll1 end1
 do
     case $action in
         run)
-            curl -X POST localhost:8080/fibonacci/100
-            echo "\n"
+            content=$(curl -X POST localhost:8080/fibonacci/100)
+            id=$(jq -r '.id' <<<"$content")
+            jq <<< "$content"
             ;;
         poll)
-            echo "enter polling id: "
-            read value
-            echo "\n"
-            curl -X GET localhost:8080/fibonacci/polling/$value
-            echo "\n"
+            content=$(curl -X GET localhost:8080/fibonacci/polling/$id)
+            jq <<< "$content"
             ;;
         end)
+            content=$(curl -X GET localhost:8080/fibonacci/$id)
+            jq <<< "$content"
+            ;;
+        run1)
+            echo "enter fibonacci position to compute: "
+            read value
+            content=$(curl -X POST localhost:8080/fibonacci/$value)
+            id=$(jq -r '.id' <<<"$content")
+            jq <<< "$content"
+            ;;
+
+        poll1)
+            echo "enter polling id: "
+            read value
+            content=$(curl -X GET localhost:8080/fibonacci/polling/$value)
+            jq <<< "$content"
+            ;;
+        end1)
             echo "enter id: "
             read value
-            echo "\n"
-            curl -X GET localhost:8080/fibonacci/$value
-            echo "\n"
+            content=$(curl -X GET localhost:8080/fibonacci/$value)
+            jq <<< "$content"
             ;;
         *)
            echo "Ooops";;
